eb0788394c533796280c7ad43ee2961e6297b583
Dubjay18 <jejeniyi7@gmail.com>
2023-07-11 22:42:42 +0100

    feat:added printlist fn

--- linked_lists/creation.js
+++ linked_lists/creation.js
@@ -20,7 +20,15 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
-
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
   append(value) {
     const newNode = {
       value,
@@ -46,5 +54,5 @@ myLinkedList2.append(5);
 myLinkedList2.append(16);
 myLinkedList2.prepend(1);
 myLinkedList2.prepend(7);
-
-console.log(myLinkedList2);
+myLinkedList2.printList();
+console.log(myLinkedList2.printList());
