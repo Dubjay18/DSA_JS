3fb3bf7776db86fc77f04aaa68ff148bf2423722
Dubjay18 <jejeniyi7@gmail.com>
2023-07-12 23:55:41 +0100

    feat:doubly linked list append

--- linked_lists/creation.js
+++ linked_lists/creation.js
@@ -85,11 +85,36 @@ class LinkedList {
     return this.printList();
   }
 }
+class DoublyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+      prev: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = {
+      value,
+      next: null,
+      prev: null,
+    };
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+}
 const myLinkedList2 = new LinkedList(10);
+const Dlinked = new DoublyLinkedList(10);
 myLinkedList2.append(5);
+Dlinked.append(5);
 myLinkedList2.append(16);
 myLinkedList2.prepend(1);
 myLinkedList2.prepend(7);
 myLinkedList2.printList();
-console.log(myLinkedList2.insert(2, 99));
+console.log(Dlinked.append(5));
 console.log(myLinkedList2.remove(5));
