008ec8de2974a8a3cd639132dbdea20a7a8bcb09
Dubjay18 <jejeniyi7@gmail.com>
2023-08-05 00:42:51 +0100

    selection sort

---
+++ sorting/main.js
@@ -0,0 +1,31 @@
+function bubble_sort(arr) {
+  const length = arr.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  console.log(arr);
+}
+
+bubble_sort([9, 10, 99, 87, 52]);
+
+function selectionSort(array) {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    let min = i;
+    let temp = array[i];
+    for (let j = 0; j < length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    array[i] = array[min];
+    array[min] = temp;
+  }
+  return array;
+}
