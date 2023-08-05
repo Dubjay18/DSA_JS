7c59bb280b4fdf22e6a1c60be6715c9afcbf2dd8
Dubjay18 <jejeniyi7@gmail.com>
2023-08-05 13:46:26 +0100

    sorting exercise

---
+++ sorting/exe.js
@@ -0,0 +1,24 @@
+//#1 - Sort 10 schools around your house by distance:
+//  insertionSort
+
+//#2 - eBay sorts listings by the current Bid amount:
+// radix or counting sort
+
+//#3 - Sport scores on ESPN
+// Quick sort
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+//Merge sort
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+//Insertion sort
+
+//#6 - Temperature Records for the past 50 years in Canada
+// radix or counting sort
+// Quick sort
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+//Quick sort
+
+//#8 - You want to teach sorting for the first time
+//Bubble sort, Selection Sort
