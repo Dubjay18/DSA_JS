ca7df0f6fbbacffd65d7f7fd3c0fc854a091b9b3
Dubjay18 <jejeniyi7@gmail.com>
2023-08-05 01:00:13 +0100

    insertion sort

--- sorting/main.js
+++ sorting/main.js
@@ -29,3 +29,20 @@ function selectionSort(array) {
   }
   return array;
 }
+
+function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < array[0]) {
+      array.unshift(array.splice(i, 1)[0]);
+    } else {
+      for (let j = 1; j < i; j++) {
+        if (
+          array[i] > array[j - 1] &&
+          array[i] < array[j]
+        ) {
+          array.splice(j, 0, array.splice(i, 1)[0]);
+        }
+      }
+    }
+  }
+}
