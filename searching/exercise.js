c556ff37fffb89757a68eb7f3211fde1ef88bc84
Dubjay18 <jejeniyi7@gmail.com>
2023-08-09 01:07:35 +0100

    searching exercise

---
+++ searching/exercise.js
@@ -0,0 +1,17 @@
+//If you know a solution is not far from the toot of the tree
+//BFS
+
+//if the three is very deep and soolutions are rare:
+//BFS
+
+//if the tree is very wide
+//DFS
+
+//if solutions are frequent but located deep in the tree:
+//DFS
+
+//determing whether a path exists between two nodes:
+//DFS
+
+//Finding the shortest path:
+//BFS
